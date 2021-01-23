44be65e5f80d73ffa7ac2e4e0f4acdc03481ce22
Evgeny Kuksov <>
2021-01-24 01:19:42 +0700

    gradle configured

--- presentation/src/main/java/com/evgenykuksov/rercipes/MainActivity.kt
+++ presentation/src/main/java/com/evgenykuksov/rercipes/MainActivity.kt
@@ -3,11 +3,10 @@ package com.evgenykuksov.rercipes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
     
 }
