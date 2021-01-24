8b1111f088f90d4ef16454fbce492d36c59f711c
Evgeny Kuksov <>
2021-01-25 00:11:26 +0700

    - added buildSrc module -app gradle to gradle.kts

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,43 @@
+object Apps {
+    const val compileSdk = 30
+    const val appId = "com.evgenykuksov.rercipes"
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val testInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
+}
+
+object Versions {
+    const val kotlin = "1.4.21"
+    const val gradle = "4.1.1"
+    const val coreKtx = "1.3.2"
+    const val appCompat = "1.0.2"
+    const val material = "1.2.1"
+
+    // view
+    const val constraintLayout = "2.0.4"
+
+    // test
+    const val junit = "4.12"
+    const val junitTest = "1.1.2"
+    const val espressoCore = "3.3.0"
+}
+
+object Libs {
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
+
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+
+    // view
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+    // test
+    const val junit = "junit:junit:${Versions.junit}"
+    const val junits = "androidx.test.ext:junit:${Versions.junitTest}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
+}
---
+++ presentation/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(Apps.compileSdk)
+    defaultConfig {
+        applicationId(Apps.appId)
+        minSdkVersion(Apps.minSdk)
+        targetSdkVersion(Apps.targetSdk)
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-dev"
+//            signingConfig = signingConfigs.getByName("debug")
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            isShrinkResources = true
+            isZipAlignEnabled = true
+            isJniDebuggable = false
+            isRenderscriptDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Libs.kotlin)
+    implementation(Libs.coreKtx)
+
+    implementation(Libs.appcompat)
+    implementation(Libs.material)
+    implementation(Libs.constraintlayout)
+
+    testImplementation(Libs.junit)
+    androidTestImplementation(Libs.junits)
+    androidTestImplementation(Libs.espressoCore)
+}
