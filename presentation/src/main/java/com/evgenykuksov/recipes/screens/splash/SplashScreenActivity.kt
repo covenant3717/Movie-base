286dbe8f04be84798e1226acfaca94a2529ad6a2
Evgeny Kuksov <>
2021-02-06 22:25:01 +0700

    add packages in presentation module

--- presentation/src/main/java/com/evgenykuksov/recipes/screens/splash/SplashScreenActivity.kt
+++ presentation/src/main/java/com/evgenykuksov/recipes/screens/splash/SplashScreenActivity.kt
@@ -1,9 +1,10 @@
-package com.evgenykuksov.recipes
+package com.evgenykuksov.recipes.screens.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.evgenykuksov.recipes.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class SplashScreenActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
