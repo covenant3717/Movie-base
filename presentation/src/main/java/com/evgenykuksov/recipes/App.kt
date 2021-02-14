ecb2c369ce0bc754068cd598a61ea92eeba25463
Evgeny Kuksov <>
2021-02-15 00:25:16 +0700

    - init koin

--- presentation/src/main/java/com/evgenykuksov/recipes/App.kt
+++ presentation/src/main/java/com/evgenykuksov/recipes/App.kt
@@ -1,13 +1,21 @@
 package com.evgenykuksov.recipes
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
+        initKoin()
     }
 
+    private fun initKoin() {
+        startKoin {
+            androidContext(this@App)
+            // declare modules
+        }
+    }
 }
 
