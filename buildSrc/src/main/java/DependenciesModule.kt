030b2cc8e00b29322315eb3a326b9bea7cc1244b
Evgeny Kuksov <44593466+covenant3717@users.noreply.github.com>
2021-10-10 19:13:14 +0700

    [Core] small refactoring

--- buildSrc/src/main/java/DependenciesModule.kt
+++ buildSrc/src/main/java/DependenciesModule.kt
@@ -86,4 +86,45 @@ object DependenciesModule {
         "androidTestImplementation"(Dependencies.Tests.junits)
         "androidTestImplementation"(Dependencies.Tests.espressoCore)
     }
+
+    private fun DependencyHandlerScope.commonFeatureDependencies() {
+        "implementation"(project(Dependencies.Modules.CORE))
+        "implementation"(project(Dependencies.Modules.DOMAIN))
+        "implementation"(project(Dependencies.Modules.DATA))
+
+        "implementation"(Dependencies.kotlin)
+        "implementation"(Dependencies.coreKtx)
+        "implementation"(Dependencies.coil)
+        "implementation"(Dependencies.coilGif)
+
+        "implementation"(Dependencies.Jetpack.viewmodel)
+        "implementation"(Dependencies.Jetpack.livedata)
+        "implementation"(Dependencies.Jetpack.runtime)
+
+        "implementation"(Dependencies.UI.appcompat)
+        "implementation"(Dependencies.UI.material)
+        "implementation"(Dependencies.UI.constraintLayout)
+        "implementation"(Dependencies.UI.fragment)
+        "implementation"(Dependencies.UI.fragmentKts)
+        "implementation"(Dependencies.UI.groupie)
+        "implementation"(Dependencies.UI.groupieExt)
+        "implementation"(Dependencies.UI.circleImageView)
+        "implementation"(Dependencies.UI.shimmer)
+        "implementation"(Dependencies.UI.photoView)
+
+        "implementation"(Dependencies.Di.koin)
+        "implementation"(Dependencies.Di.koinAndroid)
+
+        "implementation"(Dependencies.Coroutines.coroutine)
+        "implementation"(Dependencies.Coroutines.coroutineAndroid)
+
+        "testImplementation"(Dependencies.Tests.junit)
+        "androidTestImplementation"(Dependencies.Tests.junits)
+        "androidTestImplementation"(Dependencies.Tests.espressoCore)
+    }
+
+//    fun DependencyHandlerScope.featureSplash() {
+//        "implementation"(project(Dependencies.Modules.PRESENTATION)) // todo: remove after integrate Jetpack navigation
+//        commonFeatureDependencies()
+//    }
 }
