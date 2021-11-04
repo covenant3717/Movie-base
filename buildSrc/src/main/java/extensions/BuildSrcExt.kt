f2310b82d9b620fbe23fc1ba371aa9e20518a9be
Evgeny Kuksov <44593466+covenant3717@users.noreply.github.com>
2021-11-04 20:48:16 +0700

    [buildSrc] added extensions

---
+++ buildSrc/src/main/java/extensions/BuildSrcExt.kt
@@ -0,0 +1,17 @@
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+/*
+fun DependencyHandler.implementation(dependency: String) =
+    add("implementation", dependency)
+
+fun DependencyHandler.implementation(projectDependency: ProjectDependency) =
+    add("implementation", projectDependency)
+
+fun DependencyHandler.debugImplementation(dependencyNotation: Any): Dependency? =
+    add("debugImplementation", dependencyNotation)
+
+fun DependencyHandler.releaseImplementation(dependencyNotation: Any): Dependency? =
+    add("releaseImplementation", dependencyNotation)
+*/
