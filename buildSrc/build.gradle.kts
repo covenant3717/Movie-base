c6de3a0132b99046c7888bb9fa48dd76ca203189
Evgeny Kuksov <44593466+covenant3717@users.noreply.github.com>
2021-12-23 19:47:42 +0700

    [buildSrc] up gradle version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     // This should be in-sync with the "GRADLE" version exposed by "Versions"
-    implementation("com.android.tools.build:gradle:7.0.3")
+    implementation("com.android.tools.build:gradle:7.0.4")
 
     // This should be in-sync with the "KOTLIN" version exposed by "Versions"
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
