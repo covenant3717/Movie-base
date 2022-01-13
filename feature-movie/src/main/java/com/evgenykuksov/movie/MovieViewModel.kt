c8d6c71a63e109aefdd454cc8b952be4612b63ea
Evgeny Kuksov <44593466+covenant3717@users.noreply.github.com>
2022-01-13 10:56:54 +0700

    [Movie] replaced TitleItem to CustomTextItem

--- feature-movie/src/main/java/com/evgenykuksov/movie/MovieViewModel.kt
+++ feature-movie/src/main/java/com/evgenykuksov/movie/MovieViewModel.kt
@@ -1,21 +1,18 @@
 package com.evgenykuksov.movie
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.viewModelScope
 import coil.ImageLoader
 import com.evgenykuksov.domain.movies.MoviesUseCase
 import com.evgenykuksov.domain.movies.model.Actor
 import com.evgenykuksov.domain.movies.model.FullMovieData
 import com.evgenykuksov.core.base.BaseViewModel
-import com.evgenykuksov.core.items.CustomEmptyItem
-import com.evgenykuksov.core.items.CustomSingleLineLoadingItem
-import com.evgenykuksov.core.items.ErrorItem
-import com.evgenykuksov.core.items.DescriptionItem
+import com.evgenykuksov.core.items.*
 import com.evgenykuksov.movie.items.*
 import com.evgenykuksov.movie.items.ActorLoadingItem
 import com.evgenykuksov.movie.items.CastItem
 import com.evgenykuksov.movie.items.GenreItem
 import com.evgenykuksov.movie.items.RatingItem
-import com.evgenykuksov.movie.items.TitleItem
 import com.evgenykuksov.movie.navigation.MovieNavigation
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.coroutines.flow.*
@@ -65,17 +62,17 @@ class MovieViewModel(
 
     private fun buildLoadingItems(): List<Item> = listOf(
         CustomEmptyItem(R.dimen.dimen_20),
-        TitleItem(R.string.item_title_rate),
+        buildTitleItem(R.string.item_title_rate),
         CustomEmptyItem(R.dimen.dimen_8),
         CustomSingleLineLoadingItem(R.dimen.dimen_80, R.dimen.dimen_20, R.dimen.dimen_20, R.dimen.dimen_20),
         CustomEmptyItem(R.dimen.dimen_20),
 
-        TitleItem(R.string.item_title_genre),
+        buildTitleItem(R.string.item_title_genre),
         CustomEmptyItem(R.dimen.dimen_8),
         CustomSingleLineLoadingItem(null, R.dimen.dimen_20, R.dimen.dimen_20, R.dimen.dimen_20),
         CustomEmptyItem(R.dimen.dimen_20),
 
-        TitleItem(R.string.item_title_description),
+        buildTitleItem(R.string.item_title_description),
         CustomEmptyItem(R.dimen.dimen_8),
         CustomSingleLineLoadingItem(null, R.dimen.dimen_20, R.dimen.dimen_20, R.dimen.dimen_20),
         CustomEmptyItem(R.dimen.dimen_8),
@@ -86,7 +83,7 @@ class MovieViewModel(
         CustomSingleLineLoadingItem(R.dimen.dimen_100, R.dimen.dimen_20, R.dimen.dimen_20, R.dimen.dimen_20),
         CustomEmptyItem(R.dimen.dimen_20),
 
-        TitleItem(R.string.item_title_cast),
+        buildTitleItem(R.string.item_title_cast),
         CustomEmptyItem(R.dimen.dimen_8),
         CastItem(buildActorLoadingItems()),
         CustomEmptyItem(R.dimen.dimen_32)
@@ -96,22 +93,22 @@ class MovieViewModel(
 
     private fun buildItems(data: FullMovieData): List<Item> = listOf(
         CustomEmptyItem(R.dimen.dimen_20),
-        TitleItem(R.string.item_title_rate),
+        buildTitleItem(R.string.item_title_rate),
         CustomEmptyItem(R.dimen.dimen_8),
         RatingItem(data.movieDetails.voteAverage, data.movieDetails.voteCount),
         CustomEmptyItem(R.dimen.dimen_20),
 
-        TitleItem(R.string.item_title_genre),
+        buildTitleItem(R.string.item_title_genre),
         CustomEmptyItem(R.dimen.dimen_8),
         GenreItem(data.movieDetails.genres),
         CustomEmptyItem(R.dimen.dimen_20),
 
-        TitleItem(R.string.item_title_description),
+        buildTitleItem(R.string.item_title_description),
         CustomEmptyItem(R.dimen.dimen_8),
         DescriptionItem(data.movieDetails.overview),
         CustomEmptyItem(R.dimen.dimen_20),
 
-        TitleItem(R.string.item_title_cast),
+        buildTitleItem(R.string.item_title_cast),
         CustomEmptyItem(R.dimen.dimen_8),
         CastItem(buildActorItems(data.listActor)),
         CustomEmptyItem(R.dimen.dimen_32),
@@ -139,6 +136,14 @@ class MovieViewModel(
             }
         }
         .toList()
+
+    private fun buildTitleItem(@StringRes textRes: Int) = CustomTextItem(
+        textContentRes = textRes,
+        styleRes = R.style.TextAppearance_MaterialComponents_Subtitle1,
+        colorRes = R.color.item_title,
+        startPaddingRes = R.dimen.dimen_20,
+        endPaddingRes = R.dimen.dimen_20,
+    )
 
     companion object {
 
