ab73ab729cc4cf0a7d4fb57a68b07c490c224c1d
Evgeny Kuksov <44593466+covenant3717@users.noreply.github.com>
2022-01-23 16:57:24 +0700

    [Data] added LanguageInterceptor

--- data/src/main/java/com/evgenykuksov/data/di/NetworkModule.kt
+++ data/src/main/java/com/evgenykuksov/data/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.evgenykuksov.data.di
 import com.evgenykuksov.data.BuildConfig
 import com.evgenykuksov.data.network.interceptors.ApiKeyInterceptor
 import com.evgenykuksov.data.network.interceptors.HeaderInterceptor
+import com.evgenykuksov.data.network.interceptors.LanguageInterceptor
 import com.google.gson.GsonBuilder
 import com.readystatesoftware.chuck.ChuckInterceptor
 import okhttp3.OkHttpClient
@@ -29,6 +30,7 @@ internal fun networkModule(baseUrl: String, apiVersion: String, apiKey: String) 
             .readTimeout(OKHTTP_READ_TIMEOUT_MS, TimeUnit.MILLISECONDS)
             .pingInterval(OKHTTP_PING_INTERVAL_MS, TimeUnit.MILLISECONDS)
             .addInterceptor(ApiKeyInterceptor(apiKey))
+            .addInterceptor(LanguageInterceptor())
             .addInterceptor(ChuckInterceptor(this.androidContext()))
             .addInterceptor(HeaderInterceptor())
             .addInterceptor(
--- data/src/main/java/com/evgenykuksov/data/network/interceptors/LanguageInterceptor.kt
+++ data/src/main/java/com/evgenykuksov/data/network/interceptors/LanguageInterceptor.kt
@@ -3,18 +3,19 @@ package com.evgenykuksov.data.network.interceptors
 import okhttp3.Interceptor
 import okhttp3.Response
 
-internal class ApiKeyInterceptor(private val apiKey: String) : Interceptor {
+internal class LanguageInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
         val originalHttpUrl = chain.request().url
-        val newUrl = originalHttpUrl.newBuilder().addQueryParameter(QUERY_API_KEY, apiKey).build()
+        val newUrl = originalHttpUrl.newBuilder().addQueryParameter(QUERY_LANGUAGE, QUERY_LANGUAGE_VALUE).build()
         request.url(newUrl)
         return chain.proceed(request.build())
     }
 
     companion object {
 
-        private const val QUERY_API_KEY = "api_key"
+        private const val QUERY_LANGUAGE = "language"
+        private const val QUERY_LANGUAGE_VALUE = "ru"
     }
 }
