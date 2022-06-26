6be644b3c27aa0b15dbd24f2ab76bd9c834eea78
Evgeny Kuksov <44593466+covenant3717@users.noreply.github.com>
2022-06-26 19:40:02 +0300

    [BuildSrc] updated kotlin plugin deps

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -12,7 +12,7 @@ object Versions {
     }
 
     object Main {
-        const val KOTLIN = "1.5.31"
+        const val KOTLIN = "1.7.0"
         const val CORE_KTX = "1.3.2"
         const val GRADLE = "7.2.1"
         val JAVA = JavaVersion.VERSION_1_8
