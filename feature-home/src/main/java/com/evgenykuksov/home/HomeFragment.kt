b942bec7384f91299d0b41efc5e475e57c6a2072
Evgeny Kuksov <44593466+covenant3717@users.noreply.github.com>
2022-07-07 16:14:40 +0300

    test4

--- feature-home/src/main/java/com/evgenykuksov/home/HomeFragment.kt
+++ feature-home/src/main/java/com/evgenykuksov/home/HomeFragment.kt
@@ -69,7 +69,6 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
     }
 
     override fun observeState() {
-  
         viewModel.state.collectLA(viewLifecycleOwner) {
             when (it.grouping) {
                 MoviesGrouping.Linear -> {
