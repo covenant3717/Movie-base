2f3e438f331acf6685e6f996a3a7301a475944c8
Evgeny Kuksov <covenant3717@gmail.com>
2022-11-14 23:27:55 +0300

    [App] migrated SplashScreenActivity to compose

--- app/src/main/java/com/evgenykuksov/moviebase/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/evgenykuksov/moviebase/ui/splashscreen/SplashScreenActivity.kt
@@ -2,17 +2,18 @@ package com.evgenykuksov.moviebase.ui.splashscreen
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.evgenykuksov.core.extensions.launchWhenStarted
-import com.evgenykuksov.moviebase.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.lifecycleScope
 import com.evgenykuksov.moviebase.ui.main.MainActivity
 import kotlinx.coroutines.delay
 
-internal class SplashScreenActivity : AppCompatActivity(R.layout.activity_splash_screen) {
+internal class SplashScreenActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        launchWhenStarted {
+        setContent {}
+        lifecycleScope.launchWhenStarted {
             delay(DELAY_START_MAIN)
             startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
             finish()
