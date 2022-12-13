df11f70a0ce3016a840532a71dc5914c6757ca88
Evgeny Kuksov <covenant3717@gmail.com>
2022-12-13 22:27:55 +0300

    [Core] added Color.kt for compose

---
+++ core/src/main/java/com/evgenykuksov/core/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.evgenykuksov.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object ThemeColors {
+    val white100 = Color(0xFFFFFFFF)
+    val white95 = Color(0xF2FFFFFF)
+    val white90 = Color(0xE6FFFFFF)
+    val white80 = Color(0xCCFFFFFF)
+    val white70 = Color(0xB3FFFFFF)
+    val white60 = Color(0x99FFFFFF)
+    val white50 = Color(0x80FFFFFF)
+    val white40 = Color(0x66FFFFFF)
+    val white30 = Color(0x4DFFFFFF)
+    val white20 = Color(0x33FFFFFF)
+    val white10 = Color(0x1AFFFFFF)
+
+    val black100 = Color(0xFF000000)
+    val black90 = Color(0xE6000000)
+    val black80 = Color(0xCC000000)
+    val black70 = Color(0xB3000000)
+    val black60 = Color(0x99000000)
+    val black50 = Color(0x80000000)
+    val black40 = Color(0x66000000)
+    val black30 = Color(0x4D000000)
+    val black20 = Color(0x33000000)
+    val black10 = Color(0x1A000000)
+}
